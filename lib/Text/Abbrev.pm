0acbcf61f836aa4d15dedaea087bad15fff88917
GlitchMr <glitchmr@myopera.com>
2012-07-20 12:25:14 +0200

    First release of Text::Abbrev for Perl 6

---
+++ lib/Text/Abbrev.pm
@@ -0,0 +1,21 @@
+module Abbrev;
+multi sub abbrev (*@words) is export {
+    my %seen;
+    my %result;
+    for @words {
+        loop (my $len = .chars; $len > 0; --$len) {
+            my $abbrev = .substr(0, $len);
+            if %seen{$abbrev} {
+                %result.delete: $abbrev;
+            }
+            else {
+                %seen{$abbrev} = 1;
+                %result{$abbrev} = $_;
+            }
+        }
+    }
+    for @words {
+        %result{$_} = $_;
+    }
+    return %result;
+}
