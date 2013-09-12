97948ccf01c0cc55d20ff3d2813e3da6c4026c49
Konrad Borowski <glitchmr@myopera.com>
2013-09-12 14:22:30 +0200

    Use "for" loop instead of "loop".
    I believe "for" caused problems previously, but they shouldn't exist anymore.

--- lib/Text/Abbrev.pm
+++ lib/Text/Abbrev.pm
@@ -3,7 +3,7 @@ multi sub abbrev (*@words) is export {
     my $seen = KeySet.new;
     my %result;
     for @words {
-        loop (my $len = .chars; $len > 0; --$len) {
+        for 1 .. .chars -> $len {
             my $abbrev = .substr(0, $len);
             if $seen{$abbrev} {
                 %result.delete: $abbrev;
